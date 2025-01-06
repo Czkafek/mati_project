8772e6f31f42495cd12e3f5ca6e2117d54cfad08
Czkafek <czkafek@gmail.com>
2025-01-06 22:07:24 +0100

    szybka poprawka

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar/Navbar.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import HomePage from './pages/homepage/HomePage.jsx'
 import About from './pages/about/About.jsx'
-import Contact from './pages/Contact/Contact.jsx'
+import Contact from './pages/contact/Contact.jsx'
 
 function App() {
 
