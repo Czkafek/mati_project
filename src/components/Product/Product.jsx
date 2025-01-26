1ee2f52e156f8152f7507b6c8089af2ff6088321
Czkafek <czkafek@gmail.com>
2025-01-26 12:21:11 +0100

    quick product repair

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,7 +4,6 @@ import carImg from '../../assets/Products/car.jpg'
 import arrowImg from '../../assets/Products/arrow.png'
 
 export default function Product({name, img, popular}) {
-    console.log(img);
     return <div className={styles.product}>
         <h4>{name}</h4>
         <img src={carImg} className={styles.productImg} />
