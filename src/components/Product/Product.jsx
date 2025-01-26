9fc5f8d56330b2ab441f2bac3cb134e3a48a8cce
Czkafek <czkafek@gmail.com>
2025-01-26 11:44:59 +0100

    test products

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,15 +1,16 @@
 import styles from './Product.module.css';
 
 import carImg from '../../assets/Products/car.jpg'
+import arrowImg from '../../assets/arrow.png'
 
 export default function Product({name, img, popular}) {
-
+    console.log(img);
     return <div className={styles.product}>
         <h4>{name}</h4>
-        <img src={`/src/assets/Products/${img}.png`} className={styles.productImg} />
+        <img src={carImg} className={styles.productImg} />
         <p className={styles.details}>
             Szczegóły
-            <img src="/src/assets/Products/arrow.png" className={styles.arrow} />    
+            <img src={arrowImg} className={styles.arrow} />    
         </p>
     </div>
 }
